5f0d86b0ff3f2eae8b6d39f44394986d0577569d
Mikita Asotski <masotski@exadel.com>
2018-08-22 17:48:25 +0300

    add listener to ul (container) and callback to it; import class SubMenu;

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import {MainMenu} from "./component/main-menu/main-menu.js";
-
+import {SubMenu} from "./component/sub-menu/sub-menu.js";
 const container = document.querySelector('.main-menu__list');
 const menu = new MainMenu(container);
 console.log(menu);
 menu.getBrands();
+container.addEventListener('click', (evt) => {
+  const subMenu = new SubMenu(menu.getShopData(evt.target));
+});
