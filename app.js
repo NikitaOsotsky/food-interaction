e01c4854fcd3b178b794c433c5f2638edceb1cc8
Mikita Asotski <masotski@exadel.com>
2018-08-23 15:10:08 +0300

    add change subMenuContainer destination;

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import {MainMenu} from "./component/main-menu/main-menu.js";
 import {SubMenu} from "./component/sub-menu/sub-menu.js";
 const container = document.querySelector('.main-menu__list');
 const menu = new MainMenu(container);
-const subMenuContainer = document.querySelector('.sub-menu__list');
+const subMenuContainer = document.querySelector('.sub-menu');
 const subMenu = new SubMenu(subMenuContainer);
 menu.getBrands();
 container.addEventListener('click', (evt) => {
