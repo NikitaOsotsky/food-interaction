331332818ce3675c8ee5e670ed44af2e214c567f
Mikita Asotski <masotski@exadel.com>
2018-08-23 12:44:01 +0300

    export subMenu variable;

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ import {MainMenu} from "./component/main-menu/main-menu.js";
 import {SubMenu} from "./component/sub-menu/sub-menu.js";
 const container = document.querySelector('.main-menu__list');
 const menu = new MainMenu(container);
-console.log(menu);
+const subMenuContainer = document.querySelector('.sub-menu__list');
+const subMenu = new SubMenu(subMenuContainer);
 menu.getBrands();
 container.addEventListener('click', (evt) => {
-  const subMenuContainer = document.querySelector('.sub-menu__list');
-  const subMenu = new SubMenu(menu.getShopData(evt.target), subMenuContainer);
+  menu.getShopData(evt.target);
 });
+export {subMenu};
