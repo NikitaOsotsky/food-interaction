bf1464da88df227269994f7ca0e45219c407cf55
Mikita Asotski <masotski@exadel.com>
2018-08-23 15:13:15 +0300

    modefy function render to draw all elements in container;

--- component/sub-menu/sub-menu.js
+++ component/sub-menu/sub-menu.js
@@ -2,8 +2,13 @@ class SubMenu {
   constructor(container) {
     this.container = container;
   }
-  render(data) {
+  render(data, name, logo) {
     this.html = '';
+    this.html +=
+        `<img class="sub-menu__logo" src="${logo}" alt="${name}">
+         <h3 class="sub-menu__header main-header">${name}</h3>
+         <h4 class="sub-menu__header slave-header">${name}</h4>
+         <ul class=".sub-menu__list">`;
     for (let key in data[0].menu) {
       for (let item in data[0].menu[key]) {
         this.html +=
@@ -12,6 +17,7 @@ class SubMenu {
             </li>`;
       }
     }
+    this.html += `</ul>`;
     this.container.innerHTML = this.html;
   }
 
