fd1fae616ecddff56d7b8887da86d95ed58ee94a
Mikita Asotski <masotski@exadel.com>
2018-08-24 12:02:58 +0300

    add subMenuState flag;

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const container = document.querySelector('.main-menu__list');
 const menu = new MainMenu(container);
 const subMenuContainer = document.querySelector('.sub-menu');
 const subMenu = new SubMenu(subMenuContainer);
+let subMenuState = false;
 menu.getBrands();
 container.addEventListener('click', (evt) => {
+  if (!subMenuState) {
+    subMenuContainer.style.opacity = '1';
+    subMenuContainer.style.width = '100%';
+    subMenuState = true;
+  }
   menu.getShopData(evt.target);
 });
 export {subMenu};
