14ccdce48b3f7f40453f40d1e0c2c6275d1bd0af
Nikita Osotskiy <osotskiy@gmail.com>
2018-08-26 20:55:19 +0300

    add import choseElems; restructurate callback function in container.listener;

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import {MainMenu} from "./component/main-menu/main-menu.js";
 import {SubMenu} from "./component/sub-menu/sub-menu.js";
+import {choseElems} from "./component/sub-menu/sub-menu.js";
 
 const container = document.querySelector('.main-menu__list');
 const menu = new MainMenu(container);
@@ -14,12 +15,15 @@ export let subMenuState = {stage: false,
 
 menu.getBrands();
 container.addEventListener('click', (evt) => {
-  if (!subMenuState.stage) {
-    subMenuContainer.style.opacity = '1';
-    subMenuContainer.style.width = '100%';
-    subMenuState.setStage(true);
+  if (menu.getShopData(evt.target)) {
+    if (!subMenuState.stage) {
+      subMenuContainer.style.opacity = '1';
+      subMenuContainer.style.width = '100%';
+      subMenuState.setStage(true);
+    }
   }
-  menu.getShopData(evt.target);
 });
 subMenu.addListeners(subMenuContainer);
+choseElems.init();
+
 export {subMenu};
